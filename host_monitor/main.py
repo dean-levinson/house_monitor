d30919bd13ee600ac7ceead7bd97123431fc9889
dean <deanlevin99@gmail.com>
2020-09-29 13:36:51 +0300

    development

---
+++ host_monitor/main.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+async def ScanTask(parameter_list):
+    pass
+
+async def UpdateResults(parameter_list):
+    pass
+
+def main():
+    pass
+    
+
+if __name__ == "__main__":
+    main()
